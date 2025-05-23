a6fb0c7b0821c293cd59cf3447312f9798d9ad25
Haroon <hoorankhan123ff@gmail.com>
2025-05-23 20:13:46 +0500

    Update operators.py

--- operators.py
+++ operators.py
@@ -1,1 +1,13 @@
 
+a = [1, 2, 3]
+b = a
+c = [1, 2, 3]
+
+print(a is b)  # True – same object
+print(a is c)  # False – same content, different objects
+if a is None:
+    pass
+
+my_list = [1, 2, 3]
+print(2 in my_list)  # True
+print(5 in my_list)  # False
